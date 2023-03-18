e066ed7a72a36951ba8b57514a46e22ba1a9663f
Viktor Kindrat <kindratvictor07@gmail.com>
2023-03-18 21:37:02 +0200

    update

--- src/components/BasketMenu/BasketMenu.jsx
+++ src/components/BasketMenu/BasketMenu.jsx
@@ -4,6 +4,29 @@ import BasketCard from "../BasketCard/BasketCard";
 
 function BasketMenu (props){
     let basket = props.basket;
+    let sendOrder = (basket) => {
+        let message = "Need to deliver: \n"
+
+        basket.map((item, index)=>{
+            message += index + 1 + ". `" + item.id + " " + item.name + "`\n"
+        })
+
+        fetch("http://balls-server.onrender.com/bot", {
+            method: "POST", 
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: {
+                message: message
+            },
+        })
+        .then((res)=>res.json())
+        .then((data)=> {
+            alert("Надіслано успішно")
+            console.log(data)
+        })
+        return
+    }
     return (
         <div data-shown={props.open} data-theme={props.theme} className="BasketMenu">
             <h3 className="BasketMenu-headline">Кошик:</h3>
@@ -13,6 +36,9 @@ function BasketMenu (props){
                 } 
                 {
                     (basket.length !== 0 ) ? `До сплати: ${basket.reduce((acc, value)=>{return acc + parseInt(value.price)}, 0)}${basket[0].currency}` : ""
+                }
+                {
+                    (basket.length !== 0 ) ? <button onClick={sendOrder(basket)} className="BasketMenu-btn">Сформувати замовлення</button>:""
                 }
             </div>
         </div>
