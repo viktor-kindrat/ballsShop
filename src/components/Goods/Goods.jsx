8bc56c7f0e91921da5869054170f59ded9e95a62
Viktor Kindrat <kindratvictor07@gmail.com>
2023-03-19 17:55:24 +0200

    inital commit

--- src/components/Goods/Goods.jsx
+++ src/components/Goods/Goods.jsx
@@ -1,23 +1,29 @@
-import React from "react";
+import { useMemo, useState } from "react";
 import "./Goods.css"
 
 import ProductCard from "../ProductCard/ProductCard";
 
-class Goods extends React.Component {
-    render(){
-        return (
-            <section className="Goods">
-                <h2 className="Goods-headline">Каталог товарів</h2>
-                <div className="Goods-container">
-                    {
-                        this.props.goodsData.map(item => 
-                            <ProductCard addToBasketHandler={this.props.addToBasketHandler} data={item}/>    
-                        )
-                    }
-                </div>
-            </section>
-        )
-    }
+function Goods(props) {
+    let [filterText, setFilterText] = useState("");
+    const filteredItems = useMemo(() => {
+        return props.goodsData.filter(item=>item.id.toString().includes(filterText.toLowerCase()))
+    }, [props.goodsData, filterText])
+    return (
+        <section className="Goods">
+            <h2 className="Goods-headline">Каталог товарів</h2>
+            <div className="BasketMenu-search">
+                <input type="text" onChange={e=>{setFilterText(e.target.value)}} />
+            </div>
+            <div className="Goods-container">
+                {
+                    filteredItems.map(item => 
+                        <ProductCard addToBasketHandler={props.addToBasketHandler} data={item}/>    
+                    )
+                }
+            </div>
+        </section>
+    )
+    
 }
 
 export default Goods
