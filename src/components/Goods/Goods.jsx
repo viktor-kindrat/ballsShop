eb6da9608965e786aacf1c6bd08e04fd3e3c7959
Viktor Kindrat <kindratvictor07@gmail.com>
2023-03-20 22:04:01 +0200

    styles upd. add a placeholder if any item is find

--- src/components/Goods/Goods.jsx
+++ src/components/Goods/Goods.jsx
@@ -6,19 +6,17 @@ import ProductCard from "../ProductCard/ProductCard";
 function Goods(props) {
     let [filterText, setFilterText] = useState("");
     const filteredItems = useMemo(() => {
-        return props.goodsData.filter(item=>item.id.toString().includes(filterText.toLowerCase()))
+        return props.goodsData.filter(item=>item.name.toLowerCase().includes(filterText.toLowerCase()))
     }, [props.goodsData, filterText])
     return (
         <section className="Goods">
             <h2 className="Goods-headline">Каталог товарів</h2>
-            <div className="BasketMenu-search">
-                <input type="text" onChange={e=>{setFilterText(e.target.value)}} />
+            <div className="Goods-search">
+                <input className="Goods-searchInput" placeholder="Введіть назву товару" type="text" onChange={e=>{setFilterText(e.target.value)}} />
             </div>
             <div className="Goods-container">
                 {
-                    filteredItems.map(item => 
-                        <ProductCard addToBasketHandler={props.addToBasketHandler} data={item}/>    
-                    )
+                    (filteredItems.length > 0) ? filteredItems.map(item => <ProductCard addToBasketHandler={props.addToBasketHandler} data={item}/>) : <p className="Goods-absent">Немає товару назва якого містить "{filterText}"</p>
                 }
             </div>
         </section>
