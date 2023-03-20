7b507cfe2681090e84b479973806c5b34fe7fee6
Viktor Kindrat <kindratvictor07@gmail.com>
2023-03-20 21:14:36 +0200

    popup update

--- src/components/GoodsPopup/GoodsPopup.jsx
+++ src/components/GoodsPopup/GoodsPopup.jsx
@@ -1,10 +1,41 @@
 import "./GoodsPopup.css"
 
+import cart from "./img/cart.png"
+
 function GoodsPopup(props) {
     return(
         <div data-visibility={props.visibility} className="GoodsPopup">
-            {props.data.name}
-            <button onClick={props.removerFunction}>close</button>
+            <button className="GoodsPopup-close" onClick={props.removerFunction}>
+                <span></span><span></span>
+            </button>
+            <div className="GoodsPopup-imageWrap">
+                <img className="GoodsPopup-image" src={props.data.fullImage} alt={props.data.name} />
+            </div>
+            <div className="GoodsPopup-group">
+                <div className="GoodsPopup-description">
+                    <div className="GoodsPopup-header">
+                        <h3 className="GoodsPopup-headline">{props.data.name}</h3>
+                        <p className="GoodsPopup-subheadline GoodsPopup-subheadlineCursive">{props.data.subheadline}</p>
+                        <p className="GoodsPopup-subheadline">{props.data.price + props.data.currency}</p>
+                    </div>
+                    <p className="GoodsPopup-fullDescriptiop">
+                        {props.data.description}
+                    </p>
+                    <div className="GoodsPopup-propertiesGroup">
+                        <h4 className="Goods-propertiesHeadline">Доступні кольори:</h4>
+                        <ul className="GoodsPopup-properties">
+                            {
+                                props.data.colors.map(item => (
+                                    <li>{item}</li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
+                <div className="GoodsPopup-btnContainer">
+                    <button className="GoodsPopup-btn"><img src={cart} alt="cart" />Додати до кошика</button>
+                </div>
+            </div>
         </div>
     )
 }
