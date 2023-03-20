c446c77bfb502e555b38911e743cbcd64ce46cf1
= <=>
2023-03-20 17:54:46 +0200

    morer info btn look. images update

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -8,12 +8,10 @@ function ProductCard (props) {
             <img className="ProductCard-image" src={props.data.image} alt={props.data.id + " ball card"}/>
             <h3 className="ProductCard-headline">{props.data.name}</h3>
             <p className="ProductCard-text ProductCard-price">{props.data.price + props.data.currency}</p>
-            <p className="ProductCard-text">Кольори: 
-                {
-                    props.data.colors.map(item => <span className="ProductCard-color">{item}</span>)
-                }
-            </p>
-            <button onClick={()=>props.addToBasketHandler(props.data)} className="ProductCard-buyBtn"><img src={cart} alt="cart" /> До кошика</button>
+            <div className="ProductCard-buttons">
+                <button className="ProductCard-btn ProductCard-infoBtn">Дізнатись більше</button>
+                <button onClick={()=>props.addToBasketHandler(props.data)} className="ProductCard-btn ProductCard-buyBtn"><img src={cart} alt="cart" /> </button>
+            </div>
         </div>
     )
 }
