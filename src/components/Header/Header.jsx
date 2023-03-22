32c1b35ecf13a46cd60cd0b22a16bbf5deca789d
Viktor Kindrat <kindratvictor07@gmail.com>
2023-03-22 21:38:11 +0200

    some styles changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import "./Header.css"
 
 import BasketMenu from "../BasketMenu/BasketMenu";
@@ -8,61 +8,62 @@ import cart from "./img/cart.png"
 import day from "./img/day.png"
 import night from "./img/night.png"
 
-class Header extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            openBasket: false,
-            openMenu: false
-        }
-        this.basketHandle = this.basketHandle.bind(this);
-        this.menuHandle = this.menuHandle.bind(this)
-    }
-    basketHandle (){
-        this.setState(state=>{
-            if (state.openMenu === false || window.innerWidth >= 800) {
-                return {
-                    openBasket: !state.openBasket
-                }
-            }
-        })
-    }
-    menuHandle (){
-        this.setState(state=> {
-            if (state.openBasket === false) {
-                return {
-                    openMenu: !state.openMenu
-                }
-            }
-        })
-    }
-    render(){
-        return(
-            <header className="Header" data-theme={this.props.theme}>
-                <div className="Header-logo">
-                    <img src={logo} alt="logo" className="Header-logoImage"/>
-                    <p className="Header-logoText">Abibas</p>
-                </div>
-                <nav data-mobile-view={this.state.openMenu} className="Header-nav">
-                    <div className="Header-navItem">Домашня</div>
-                    <div className="Header-navItem">Товари</div>
-                    <div className="Header-navItem">Контакти</div>
-                    <div className="Header-navItem">Про нас</div>
-                </nav>
-                <div className="Header-controlls">
-                    <button onClick={this.props.themeHandler} className="Header-btn Header-themeBtn"><img src={this.props.theme === "night" ? night : day} alt="theme switch" /></button>
-                    <button onClick={this.basketHandle} data-count={this.props.basket.length} className="Header-btn Header-cartBtn"><img src={cart} alt="cart" /></button>
-                    <button onClick={this.menuHandle} className="Header-btn Header-menuBtn">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </button>
+function Header (props){
+    let [openBasket, setOpenBasket] = useState(false)
+    let [openMenu, setOpenMenu] = useState(false)
+    let [totalTheme, setTotalTheme] = useState(props.theme);
+    let [scroll, setScroll] = useState(0);
 
-                </div>
-                <BasketMenu sendOrder={this.props.sendOrder} basketRemover={this.props.basketRemover} open={this.state.openBasket} basket={this.props.basket} theme={this.props.theme}/>
-            </header>
-        )
+    window.addEventListener("scroll", (e)=>{
+        setScroll(window.scrollY)
+    })
+
+    function basketHandle (){
+        if (openMenu === false || window.innerWidth >= 800) {
+            setOpenBasket(!openBasket)
+        }
     }
+    function menuHandle (){
+        if (openBasket === false) {
+            setOpenMenu(!openMenu)
+        }
+    }
+
+    useEffect(()=>{
+        if (props.theme !== "night"){
+            if (window.scrollY >= window.innerHeight) {
+                setTotalTheme("day")
+            } else {
+                setTotalTheme("night")
+            }
+        }
+    }, [scroll])
+    return(
+         <header id="Header" className="Header" data-theme={totalTheme}>
+            <div className="Header-logo">
+                <img src={logo} alt="logo" className="Header-logoImage"/>
+                <p className="Header-logoText">Abibas</p>
+            </div>
+            <nav data-mobile-view={openMenu} className="Header-nav">
+                <div className="Header-navItem">Домашня</div>
+                <div className="Header-navItem">Товари</div>
+                <div className="Header-navItem">Контакти</div>
+                <div className="Header-navItem">Про нас</div>
+            </nav>
+            <div className="Header-controlls">
+                <button onClick={props.themeHandler} className="Header-btn Header-themeBtn"><img src={props.theme === "night" ? night : day} alt="theme switch" /></button>
+                <button onClick={basketHandle} data-count={props.basket.length} className="Header-btn Header-cartBtn"><img src={cart} alt="cart" /></button>
+                <button onClick={menuHandle} className="Header-btn Header-menuBtn">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
+
+            </div>
+            <BasketMenu sendOrder={props.sendOrder} basketRemover={props.basketRemover} open={openBasket} basket={props.basket} theme={props.theme}/>
+        </header>
+    )
+    
 }
 
 export default Header
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import { useEffect } from 'react';
+import Typed from 'typed.js';
+
 import "./Home.css"
 
-import image from "./img/image.png"
 
-class Home extends React.Component {
-    render() {
-        return (
-            <section className="Home">
-                <div className="Home-description">
-                    <h1 className="Home-headline">Тут лише <span className="Home-headlineBlue">найкращі</span> м'ячі!</h1>
-                    <p className="Home-text">Упевнене вдосконалення, розширення асортименту товарів, синергетичне поєднання та відкритість гармонійної команди забезпечили організації успіх і провідну роль на світовому ринку.</p>
-                    <button className="Home-btn">До товарів</button>
-                </div>
-                <div className="Home-imageContainer">
-                    <img src={image} alt="The best goods here" className="Home-image" />
-                </div>
-            </section>
-        )
-    }
+function Home (props) {
+    useEffect(()=>{
+        let typed;
+        if (document.querySelector('.Home-headlineBlue')){
+            let options = {
+                strings: ['неперевершені', 'найкращі', 'суперові', 'професійні'],
+                loop: true,
+                showCursor: true,
+                cursorChar: '|',
+                loopCount: Infinity,
+                backDelay: 5000,
+                backSpeed: 100,
+                typeSpeed: 100,
+                smartBackspace: true,
+              };
+            typed = new Typed('.Home-headlineBlue', options);
+        }
+
+        return ()=>{
+            if (document.querySelector('.Home-headlineBlue')){
+                typed.destroy()
+            }
+        }
+    })
+    return (
+        <section className="Home">
+            <div className="Home-description">
+                <h1 className="Home-headline">Тут лише <span className="Home-headlineBlue">найкращі</span> м'ячі!</h1>
+                <p className="Home-text">Упевнене вдосконалення, розширення асортименту товарів, синергетичне поєднання та відкритість гармонійної команди забезпечили організації успіх і провідну роль на світовому ринку.</p>
+                <button className="Home-btn">До товарів</button>
+            </div>
+        </section>
+    )
+    
 }
 
 export default Home
